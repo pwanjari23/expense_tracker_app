96fcd6e26e057ba24d02295f16562dd725250380
pwanjari23 <pratikshawanjari23@gmail.com>
2026-02-08 19:00:44 +0530

    change in frontend

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 app.use(cors());
 const sequelize = require("./config/sequelize");
 
-const PORT = process.env.PORT || 5000;
+const PORT = APPLICATION_BACKEND_BASE_URL || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
